b0af8483b2716c3613340781971669aebb3907b8
real_name_hidden <79501145+PetraMoyle@users.noreply.github.com>
2021-04-25 10:50:37 +0800

    adding changes

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "f1be8499b0fa0ffb8bf5670adfbbd0a5"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCEcp3_pWbGtSmJM1Coxl1d6YCo_2U-O5w"
